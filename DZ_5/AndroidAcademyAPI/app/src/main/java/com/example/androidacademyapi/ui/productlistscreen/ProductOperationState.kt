42ed7fa632c493f43e53b3ede8a89f3a114cb7c5
JazoFilip <filip.jazo29@gmail.com>
2026-06-23 10:55:31 +0200

    Delete DZ_5 directory

--- DZ_5/AndroidAcademyAPI/app/src/main/java/com/example/androidacademyapi/ui/productlistscreen/ProductOperationState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.androidacademyapi.ui.productlistscreen
-
-sealed interface ProductOperationState {
-    data object Idle: ProductOperationState
-    data class Error(val message: String): ProductOperationState
-    data class Success(val message: String): ProductOperationState
-}
